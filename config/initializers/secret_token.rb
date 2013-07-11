7b5ca147ce80094fc385be14db56d4af1548fb9e
tony Ogochukwu <sej30@hotmail.com>
2013-07-11 02:58:35 +0100

    my first commit for Asim

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asim::Application.config.secret_token = '5866105e365b0e6131edcd110f6cee0b6d49c4650715f2b2583d84584e1d7891e12c98f89af98f82d1c4c273007b515306d0e4190950229c4ca14a1eb0abfd37'
